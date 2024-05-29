1ff7f09809664f8124ed3fbd9c5e8b439663b747
as2sp <you@example.com>
2024-05-29 15:57:39 +0500

    Added descriptions for all classes, methods and functions in data_processor.py.

--- models/data_processor.py
+++ models/data_processor.py
@@ -6,7 +6,16 @@ from models.pipeline import PipelineConfig
 
 
 class BaseDataProcessor:
+    """
+    Base class for data processing, containing the main methods for executing ETL processes.
+    """
     def __init__(self, config: PipelineConfig, **params: Any):
+        """
+        Initialize the base data processor.
+
+        :param config: Pipeline configuration.
+        :param params: Additional parameters for data processing.
+        """
         self.spark = SparkSession.builder \
             .appName("DataProcessorApp") \
             .config("spark.driver.extraClassPath", "db_drivers/postgresql-42.7.3.jar") \
@@ -16,23 +25,57 @@ class BaseDataProcessor:
         self.config = config
 
     def extract(self) -> Union[DataFrame, List[DataFrame]]:
+        """
+        Extract data method to be implemented by subclasses.
+
+        :return: Extracted data from source as a DataFrame or a list of DataFrames.
+        """
         pass
 
     def transform(self, df: Union[DataFrame, List[DataFrame]]) -> Union[DataFrame, List[DataFrame]]:
+        """
+        Transform data method to be implemented by subclasses.
+
+        :param df: Input DataFrame or list of DataFrames to transform.
+        :return: Transformed data as a DataFrame or a list of DataFrames.
+        """
         pass
 
     def load(self, df: Union[DataFrame, List[DataFrame]]) -> None:
+        """
+        Load data method to be implemented by subclasses.
+
+        :param df: DataFrame or list of DataFrames to load to destination.
+        """
         pass
 
     def join(self, dfs: Union[DataFrame, List[DataFrame]], joiner: Optional[Tuple] = None) -> DataFrame:
+        """
+        Join multiple DataFrames method to be implemented by subclasses.
+
+        :param dfs: DataFrame or list of DataFrames to join.
+        :param joiner: Optional tuple specifying the join function and its parameters.
+        :return: Joined DataFrame.
+        """
         pass
 
 
 class DataProcessor(BaseDataProcessor):
+    """
+    Data processor class extending the base data processor with specific ETL process implementations.
+    """
     def processor_exception(self, message: str):
+        """
+        Raise an exception with the given message.
+
+        :param message: Message for the exception.
+        """
         raise Exception(message)
 
     def run(self) -> None:
+        """
+        Run the data processing pipeline.
+        """
         data_processor_logger.debug("Starting pipeline execution")
         data_processor_logger.debug("Starting extract phase")
         dfs = self.extract()
@@ -65,6 +108,11 @@ class DataProcessor(BaseDataProcessor):
         data_processor_logger.debug("Pipeline execution finished")
 
     def extract(self) -> Union[DataFrame, List[DataFrame]]:
+        """
+        Extract data from the sources specified in the pipeline configuration.
+
+        :return: Extracted data as a DataFrame or a list of DataFrames.
+        """
         data_processor_logger.info("Extracting data")
         dfs = []
         for extractor in self.config.extractors:
@@ -75,6 +123,12 @@ class DataProcessor(BaseDataProcessor):
         return dfs[0] if len(dfs) == 1 else dfs
 
     def transform(self, df: Union[DataFrame, List[DataFrame]]) -> Union[DataFrame, List[DataFrame]]:
+        """
+        Transform the extracted data using the transformers specified in the pipeline configuration.
+
+        :param df: DataFrame or list of DataFrames to transform.
+        :return: Transformed data as a DataFrame or a list of DataFrames.
+        """
         data_processor_logger.info("Transforming data")
         if not self.config.transformers:
             return df if isinstance(df, list) else [df]
@@ -87,6 +141,11 @@ class DataProcessor(BaseDataProcessor):
         return dfs if len(dfs) > 1 else dfs[0]
 
     def load(self, df: Union[DataFrame, List[DataFrame]]) -> None:
+        """
+        Load the transformed data into the specified destinations.
+
+        :param df: DataFrame or list of DataFrames to load.
+        """
         data_processor_logger.info("Loading data")
         if isinstance(df, DataFrame):
             df.cache()
@@ -101,6 +160,13 @@ class DataProcessor(BaseDataProcessor):
         data_processor_logger.info("Data loading completed")
 
     def join(self, dfs: Union[DataFrame, List[DataFrame]], joiner: Optional[Tuple] = None) -> DataFrame:
+        """
+        Join multiple DataFrames using the specified joiner function.
+
+        :param dfs: DataFrame or list of DataFrames to join.
+        :param joiner: Optional tuple specifying the join function and its parameters.
+        :return: Joined DataFrame.
+        """
         data_processor_logger.info("Joining data")
         if not dfs:
             raise self.processor_exception("No data to join")
